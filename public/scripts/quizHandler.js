27b7ef60c5942b7fd79d792ccee856e64e8e83e5
BhanukaDev <bhanukadassanayake@gmail.com>
2023-07-24 11:01:22 +0530

    update

---
+++ public/scripts/quizHandler.js
@@ -0,0 +1,422 @@
+const wildlifeQ = {
+  typeName: 'Wildlife',
+  content: [
+    {
+      que: 'What is the national animal of Sri Lanka?',
+      a1: 'Lion',
+      a2: 'Elephant',
+      a3: 'Grizzled Squirrel',
+      a4: 'Peacock',
+      corans: 'Grizzled Squirrel',
+    },
+    {
+      que: 'Which national park in Sri Lanka is famous for its elephant population?',
+      a1: 'Yala National Park',
+      a2: 'Sinharaja Forest Reserve',
+      a3: 'Minneriya National Park',
+      a4: 'Horton Plains National Park',
+      corans: 'Yala National Park',
+    },
+  ],
+};
+
+const beaches = {
+  typeName: 'Beaches',
+  content: [
+    {
+      que: 'What is the popular surfing destination on the east coast of Sri Lanka?',
+      a1: 'Arugam Bay',
+      a2: 'Hikkaduwa',
+      a3: 'Pasikudah',
+      a4: 'Tangalle',
+      corans: 'Arugam Bay',
+    },
+    {
+      que: 'Which beach is known for its bioluminescent plankton phenomenon?',
+      a1: 'Trincomalee Beach',
+      a2: 'Arugam Bay',
+      a3: 'Kalpitiya Beach',
+      a4: 'Nilaveli Beach',
+      corans: 'Kalpitiya Beach',
+    },
+    {
+      que: 'Which beach is located near the historic Galle Fort?',
+      a1: 'Jungle Beach',
+      a2: 'Unawatuna Beach',
+      a3: 'Tangalle Beach',
+      a4: 'Hikkaduwa Beach',
+      corans: 'Unawatuna Beach',
+    },
+  ],
+};
+
+const hitoricalSites = {
+  typeName: 'Historical Sites',
+  content: [
+    {
+      que: 'Which UNESCO World Heritage Site is known for its ancient rock fortress?',
+      a1: 'Sigiriya',
+      a2: 'Dambulla Cave Temple',
+      a3: 'Gal Viharaya',
+      a4: 'Anuradhapura',
+      corans: 'Sigiriya',
+    },
+    {
+      que: 'Which site is famous for its collection of ancient Buddhist statues carved into the rock face?',
+      a1: 'Dambulla Cave Temple',
+      a2: 'Mihintale',
+      a3: 'Samadhi Statue at Mahamevnawa Park',
+      a4: 'Lankatilaka Temple',
+      corans: 'Dambulla Cave Temple',
+    },
+  ],
+};
+
+const hillCountry = {
+  typeName: 'Hill Country',
+  content: [
+    {
+      que: 'What is the highest mountain peak in Sri Lanka?',
+      a1: "Adam's Peak",
+      a2: 'Kunckels Mountain Range',
+      a3: 'Ella Rock',
+      a4: 'Pidurutalagala',
+      corans: 'Pidurutalagala',
+    },
+    {
+      que: 'Which scenic train journey is known for its breathtaking views in the Hill Country?',
+      a1: 'Colombo to Kandy',
+      a2: 'Kandy to Ella',
+      a3: 'Nuwara Eliya to Ella',
+      a4: 'Haputale to Ella',
+      corans: 'Kandy to Ella',
+    },
+  ],
+};
+
+const culturalFest = {
+  typeName: 'Cultural Festival',
+  content: [
+    {
+      que: 'Which festival celebrates the Sinhalese and Tamil New Year in Sri Lanka?',
+      a1: 'Vesak',
+      a2: 'Esala Perahera',
+      a3: 'Diwali',
+      a4: 'Avurudu',
+      corans: 'Avurudu',
+    },
+
+    {
+      que: 'What is the traditional dance form performed during cultural festivals in Sri Lanka?',
+      a1: 'Bharatanatyam',
+      a2: 'Kandyan Dance',
+      a3: 'Kolam Dance',
+      a4: 'Yakka Dance',
+      corans: 'Kandyan Dance',
+    },
+  ],
+};
+
+const teaPlantation = {
+  typeName: 'Tea Plantations',
+  content: [
+    {
+      que: 'Which region in Sri Lanka is famous for its tea plantations',
+      a1: 'Kandy',
+      a2: 'Galle',
+      a3: 'Colombo',
+      a4: 'Jaffna',
+      corans: 'Kandy',
+    },
+    {
+      que: 'What is the famous tea growing area known as "Little England"?',
+      a1: 'Badulla',
+      a2: 'Ruhuna',
+      a3: 'Uva',
+      a4: 'Nuwara Eliya',
+      corans: 'Nuwara Eliya',
+    },
+  ],
+};
+
+const adventureActivities = {
+  typeName: 'Adventure Activities',
+  content: [
+    {
+      que: 'What is the popular location for white water rafting in Sri Lanka?',
+      a1: 'Ella',
+      a2: 'Kandy',
+      a3: 'Kitulgala',
+      a4: 'Horton Plains',
+      corans: 'Kitulgala',
+    },
+    {
+      que: 'What is the popular location for whale watching in Sri Lanka?',
+      a1: 'Trincomalee',
+      a2: 'Kalpitiya',
+      a3: 'Galle',
+      a4: 'Negombo',
+      corans: 'Trincomalee',
+    },
+  ],
+};
+
+const ayurvedha = {
+  typeName: 'Ayurvedha',
+  content: [
+    {
+      que: 'What is the traditional system of medicine practiced in Sri Lanka?',
+      a1: 'Unani',
+      a2: 'Homeopathy',
+      a3: 'Ayurveda',
+      a4: 'Chinese medicine',
+      corans: 'Ayurveda',
+    },
+    {
+      que: 'What is the traditional Ayurvedic practice of cleansing called?',
+      a1: 'Basti',
+      a2: 'Shirodhara',
+      a3: 'Swedana',
+      a4: 'Panchakarma',
+      corans: 'Panchakarma',
+    },
+  ],
+};
+
+const waterFalls = {
+  typeName: 'Waterfalls',
+  content: [
+    {
+      que: 'What is the tallest waterfall in Sri Lanka?',
+      a1: 'Diyaluma Falls',
+      a2: 'Ravana Falls',
+      a3: "St.Clair's Falls",
+      a4: 'Bambarakanda Falls',
+      corans: 'Bambarakanda Falls',
+    },
+  ],
+};
+
+const cuisine = {
+  typeName: 'Cuisine',
+  content: [
+    {
+      que: 'What is the staple food of Sri Lanka?',
+      a1: 'Rice',
+      a2: 'Roti',
+      a3: 'Hoppers',
+      a4: 'Bread',
+      corans: 'Rice',
+    },
+    {
+      que: 'What is the popular Sri Lankan street food known as a savory pastry filled with spiced vegetables or meat?',
+      a1: 'Kottu Roti',
+      a2: 'Samosa',
+      a3: 'Pol Sambol',
+      a4: 'Lamprais',
+      corans: 'Samosa',
+    },
+    {
+      que: 'Which seafood dish is a specialty of the coastal regions in Sri Lanka?',
+      a1: 'Kottu Roti',
+      a2: 'Hoppers',
+      a3: 'Pol Sambol',
+      a4: 'Ambul Thiyal',
+      corans: 'Ambul Thiyal',
+    },
+  ],
+};
+
+const religion = {
+  typeName: 'Religion',
+  content: [
+    {
+      que: 'What is the predominant religion in Sri Lanka?',
+      a1: 'Hinduism',
+      a2: 'Islam',
+      a3: 'Buddhism',
+      a4: 'Christianity',
+      corans: 'Buddhism',
+    },
+
+    {
+      que: 'Which religious festival celebrates the birth, enlightenment, and death of Lord Buddha and is widely observed in Sri Lanka?',
+      a1: 'Diwali',
+      a2: 'Vesak',
+      a3: 'Ramadan',
+      a4: 'Christmas',
+      corans: 'Vesak',
+    },
+  ],
+};
+
+const totalMins = 1;
+const warningsecs = 30;
+let remainTime = totalMins * 60;
+
+let soundPlayed = false;
+
+const timerElement = document.getElementById('timer');
+const timerIcon = document.getElementsByClassName('fa-clock')[0];
+
+let quizScore = 0;
+let answeredQuestions = 0;
+let wrongQuestions = 0;
+let currentQuizIndex = 0;
+
+let correctAnswer = '';
+
+const questions = [
+  wildlifeQ,
+  beaches,
+  hitoricalSites,
+  hillCountry,
+  culturalFest,
+  teaPlantation,
+  //religion,
+  adventureActivities,
+  ayurvedha,
+  waterFalls,
+  cuisine,
+];
+
+let mainLoop;
+
+function startQuizSetup() {
+  mainLoop = setInterval(updateCountDown, 1000);
+}
+
+function stopQuizSetup() {
+  clearInterval(mainLoop);
+}
+
+const button1 = document.getElementById('ans1');
+const button2 = document.getElementById('ans2');
+const button3 = document.getElementById('ans3');
+const button4 = document.getElementById('ans4');
+
+const buttons = [button1, button2, button3, button4];
+
+function setQuestion(index) {
+  const quesEle = document.getElementById('question');
+  const ind = Math.floor(Math.random() * questions[index].content.length);
+
+  quesEle.textContent = questions[index].content[ind].que;
+
+  correctAnswer = questions[index].content[ind].corans;
+  document.getElementById('quizctTitle').textContent =
+    questions[index].typeName;
+
+  button1.textContent = button1.dataset.ans = questions[index].content[ind].a1;
+  button2.textContent = button2.dataset.ans = questions[index].content[ind].a2;
+  button3.textContent = button3.dataset.ans = questions[index].content[ind].a3;
+  button4.textContent = button4.dataset.ans = questions[index].content[ind].a4;
+}
+
+submitQuestion();
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.ans == correctAnswer;
+  answeredQuestions++;
+  if (isCorrect) {
+    selectedBtn.classList.add('correct');
+    quizScore++;
+  } else {
+    selectedBtn.classList.add('incorrect');
+    wrongQuestions++;
+  }
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+}
+
+function submitQuestion() {
+  if (currentQuizIndex < questions.length) {
+    document.getElementById('qNo').textContent = `${currentQuizIndex + 1}/${
+      questions.length
+    }`;
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove('correct');
+      buttons[i].classList.remove('incorrect');
+    }
+
+    setQuestion(currentQuizIndex);
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = false;
+    }
+
+    currentQuizIndex++;
+
+    // logic to check right answer
+  } else {
+    quizOver();
+  }
+}
+
+function quizOver() {
+  document.getElementById(
+    'questxt'
+  ).textContent = `Questions : ${questions.length}`;
+  document.getElementById('scoretxt').textContent = `Score : ${quizScore * 10}`;
+  document.getElementById(
+    'wrnganstxt'
+  ).textContent = `Wrong Answers : ${wrongQuestions}`;
+
+  document.getElementById('msdtxtx').textContent = `Missed Answers : ${
+    questions.length - answeredQuestions
+  }`;
+
+  document.getElementById('tmertxt').textContent = `Time : ${
+    totalMins * 60 - remainTime
+  }`;
+
+  document.getElementById('scoreboard').style.display = 'flex';
+  stopQuizSetup();
+}
+
+function restartQuiz() {
+  document.getElementById('scoreboard').display = 'none';
+  location.reload();
+}
+
+function updateCountDown() {
+  let mins = Math.floor(remainTime / 60);
+  let secs = remainTime % 60;
+
+  mins = mins < 0 ? '0' : mins;
+  secs = secs < 0 ? '0' : secs;
+
+  mins = mins < 10 ? '0' + mins : mins;
+  secs = secs < 10 ? '0' + secs : secs;
+
+  if (remainTime < warningsecs) {
+    timerElement.style.color = 'red';
+
+    if (!soundPlayed) {
+      playSound('assets/soundFX/511597__robpnicholson__dirty-siren.wav', true);
+      timerIcon.classList.add('fa-bounce');
+      soundPlayed = true;
+    }
+  }
+
+  if (remainTime === 0) {
+    // Timer reached 0, reset the flag to allow playing the sound again if needed
+    soundPlayed = false;
+    quizOver();
+    console.log('happen');
+  }
+
+  timerElement.textContent = `${mins}:${secs}`;
+  remainTime--;
+
+  return remainTime;
+}
+
+function playSound(path, loop = false) {
+  let audio = new Audio(path);
+  audio.autoplay = true;
+  audio.loop = loop;
+  audio.play();
+}
--- public/scripts/quizstart.js
+++ public/scripts/quizstart.js
@@ -1,11 +1,31 @@
 function startQuiz() {
   const welcomeContent = document.getElementById('welcomeContent');
   const quizContent = document.getElementById('quizContent');
+  const qNo = document.getElementById('qNo');
 
+  quizContent.style.display = 'flex';
+
+  welcomeContent.style.left = '-100%';
+  //welcomeContent.style.visibility = 'false';
+  welcomeContent.style.margin = '0px';
   setTimeout(function () {
+    quizContent.style.right = '0%';
     welcomeContent.style.display = 'none';
-    quizContent.style.display = 'flex';
+    qNo.style.display = 'inherit';
+    setMap();
   }, 500);
+
+  startQuizSetup();
 }
 
-function setQuizMap() {}
+//setMap();
+
+function setMap() {
+  const mainLand = document.getElementsByClassName('mainLand')[0];
+  const beach = document.getElementsByClassName('beach')[0];
+  const seablob = document.getElementsByClassName('seaBlob')[0];
+
+  mainLand.style.width = '250px';
+  beach.style.width = '300px';
+  seablob.style.width = '820px';
+}
